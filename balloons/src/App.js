604b96e14ed2d11d407eac05555fedbd1b53ec72
Alex Grobovsky <ktotaam@mail.ru>
2021-06-26 01:14:47 +0500

    fix: fix the way of importing firebase

--- balloons/src/App.js
+++ balloons/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from "firebase/";
+import firebase from "firebase/app";
 import 'firebase/auth';
 import useAuth from "./Components/Hooks/useAuth/useAuth";
 import NavBar from "./Components/NavBar/Navbar";
